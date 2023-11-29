b509f1446e68c00ad2c27282177d56054f7839ca
prayashshakya20@gmail.com <prayashshakya20@gmail.com>
2023-11-29 21:11:27 +0545

    add quiz repository layer, quiz controller start

---
+++ app/controllers/quizes.controller.js
@@ -0,0 +1,1 @@
+const quizRepo = require("../db/repository/quizes.repository")
--- app/db/repository/quizes.repository.js
+++ app/db/repository/quizes.repository.js
@@ -2,4 +2,13 @@ const {quizCollection} = require("../models/quizes.model")
 const {courseCollection} = require("../models/courses.model")
 const {unitCollection} = require("../models/units.model")
 
-//quizes in mcq format??
+const addNewQuiz = async(quizDetails) =>{
+    try{
+        const newDoc = await quizCollection.add(quizDetails)
+        return newDoc
+    }catch(error){
+        console.log(error)
+    }
+}
+
+module.exports = {addNewQuiz}
