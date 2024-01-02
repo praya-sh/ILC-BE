08048c86cfa16305447be9d5a3beabe67ed8eb5c
prayashshakya20@gmail.com <prayashshakya20@gmail.com>
2024-01-02 20:20:18 +0545

    email pass update

--- app/services/emailServices/eService.js
+++ app/services/emailServices/eService.js
@@ -1,35 +1,32 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 // const {successResponse} = require('../../libs/responseHelpers')
 
-const sendmail = async(toEmailArray, subject, text)=>{
-    const transporter = nodemailer.createTransport({
-    service: 'gmail',
-  auth: {
-    user: 'intrinsiclearn6@gmail.com',
-    pass: 'wqjj nmbr taeu qsiq'
-  }
-})
+const sendmail = async (toEmailArray, subject, text) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "intrinsiclearn6@gmail.com",
+      pass: "btsw fxhe zpyj igsn ",
+    },
+  });
 
-const mailOptions = {
-    from: 'intrinsiclearn6@gmail.com',
+  const mailOptions = {
+    from: "intrinsiclearn6@gmail.com",
     to: toEmailArray,
     subject: subject,
-    text
+    text,
   };
 
-
-return transporter.sendMail(mailOptions, function(error, info){
+  return transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
-      console.log("\n send email error\n",error);
-      return false
+      console.log("\n send email error\n", error);
+      return false;
     } else {
-      console.log('Email sent: ' + info.response);
-      return true
+      console.log("Email sent: " + info.response);
+      return true;
     }
-}
+  });
+};
 
-);}
-
-
-module.exports= {sendmail};
+module.exports = { sendmail };
